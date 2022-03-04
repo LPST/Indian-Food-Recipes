fff129513745a581561c9022a6394bd5d43b548c
Ebaker-TL <ebaker@thorlabs.com>
2022-03-04 15:39:38 -0500

    added comments

--- python/IndianRecipes.py
+++ python/IndianRecipes.py
@@ -17,13 +17,18 @@ def main():
 
 
 def extract_data(contents, num_rows=None):
+    """
+    a function to convert the string entries of the excel file to recipe objects
+
+    """
+    #num_rows is their for convenience. When testing a function, we don't need to load all 6000 rows. 
     labels = contents[0]
     recipes = []
-    if type(num_rows) == int:
+    if type(num_rows) == int: 
         last_row = num_rows
     else:
         last_row = len(contents)
-    for i in range(1, last_row):
+    for i in range(1, last_row): #loop through the rows, create a Recipe object, and add it to the list
         uid, name_h, name_e, ingredients_h, ingredients_e, prep, cook, total, servings, cuisine, course, diet, instructions_h, instructions_e, url = contents[i]
         recipe = Recipe(uid, name_h, name_e, ingredients_h, ingredients_e, prep, cook, total, servings, cuisine, course, diet, instructions_h, instructions_e, url)
         recipes.append(recipe)
@@ -31,6 +36,7 @@ def extract_data(contents, num_rows=None):
 
 class Recipe:
     def __init__(self, uid, name_h, name_e, ingredients_h, ingredients_e, prep, cook, total, servings, cuisine, course, diet, instructions_h, instructions_e, url):
+        #copy and paste the values, make sure they are the right types
         self.uid = uid
         self.name_h = str(name_h)
         self.name_e = str(name_e)
@@ -48,47 +54,50 @@ class Recipe:
         self.url = str(url)
 
     def __repr__(self):
+        #the internet told me this is a standard format for a __repr__ string
         return "<Recipe uid:%s \nname_h:%s \nname_e:%s \ningredients_h:%s \ningredients_e:%s \nprep:%i \ncook:%i \ntotal:%i \nservings:%i \ncuisine:%s \ncourse:%s \ndiet:%s instructions_h:%s, \ninstructions_e:%s \nurl:%s>" % (self.uid, self.name_h, self.name_e, self.ingredients_h, self.ingredients_e, self.prep, self.cook, self.total, self.servings, self.cuisine, self.course, self.diet, self.instructions_h, self.instructions_e, self.url)
 
     def search_attr(self, attr, value):
-
-        if type(getattr(self, attr)) != str:
+        """
+        This function searches the object's attribute for a given value 
+        """
+        if type(getattr(self, attr)) != str:#some basic error handling in case someone searches by prep time
             print("Sorry, search_attr literally can't handle non-string attributes right now.")
             return None
-        elif getattr(self, attr).casefold().find(value.casefold()) >= 0:
+        elif getattr(self, attr).casefold().find(value.casefold()) >= 0: #this will be -1 if the find() function doesn't
             return True
         else:
             return False
 
 def search_recipes(recipes, attr, value):
     results = []
-    for i in range(len(recipes)):
-        search_bool = recipes[i].search_attr(attr, value)
+    for i in range(len(recipes)): #for each recipe
+        search_bool = recipes[i].search_attr(attr, value) #search the recipe for the specified value
         if search_bool == False:
             pass
         else:
-            results.append(recipes[i])
+            results.append(recipes[i]) #if it's in the recipe, add it to the output
     return results
 
 def attr_counts(recipes, attr):
     #this will not distinguish between "2 cups butter" and "3 cups butter" because those strings are not identical. I think I need to use regex to get the word after the number and before the hyphen
     counts = {}
-    for i in range(len(recipes)):
-        for ingredient in getattr(recipes[i], attr).split(","):
-            if ingredient in counts.keys():
-                counts.update({ingredient:counts[ingredient] + 1})
+    for i in range(len(recipes)): #for each recipe
+        for ingredient in getattr(recipes[i], attr).split(","): #for each ingredient in the recipes' list of ingredients
+            if ingredient in counts.keys(): #if the ingredient is in the count already
+                counts.update({ingredient:counts[ingredient] + 1})#increment the count
             else:
-                counts.update({ingredient:1})
+                counts.update({ingredient:1}) #if it's not in the count, add it to the count
     return counts
 
 def highest_attr_counts(recipes, attr, n):
-    attr_dict = attr_counts(recipes, attr)
-    highest_n = [['',0]]*n
-    for key in attr_dict.keys():
-        for i in range(len(highest_n)):
-            if highest_n[i][1] < attr_dict[key]:
-                highest_n[i] = [key, attr_dict[key]]
+    attr_dict = attr_counts(recipes, attr) #get the counts
+    highest_n = [['',0]]*n #preload the results
+    for key in attr_dict.keys(): #for each ingredient in the count
+        for i in range(len(highest_n)): #check each element of the results
+            if highest_n[i][1] < attr_dict[key]: #if its count is higher than any of the results
+                highest_n[i] = [key, attr_dict[key]] #add it to the results
                 break
-    return highest_n
+    return highest_n 
 if __name__ == '__main__':
     main()
--- python/mymodule.py
+++ python/mymodule.py
@@ -6,10 +6,10 @@ def read_csv(fname):
     A helper function to read csv files. 
     """
     out = []
-    with open(fname, 'r') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            out.append(row)
+    with open(fname, 'r') as f: #open the file
+        reader = csv.reader(f) #create the reader object
+        for row in reader: #for each row in the file
+            out.append(row) #add it to the output
     return out
 
 def read_xlsx(fname, num_rows=None):
@@ -17,17 +17,17 @@ def read_xlsx(fname, num_rows=None):
     A helper function to read XSLX files. 
     """
     out = []
-    wb = xl.load_workbook( filename = fname)
+    wb = xl.load_workbook( filename = fname) #create workbook object
     sheet = wb.active #currently only reads the active sheet
-    if type(num_rows) == int:
+    if type(num_rows) == int: #num_rows allows us to select how many rows we need. Useful for testing functions on a subsection of the data rather than waiting for the whole thing to load. 
         last_row = num_rows
     else:
         last_row = sheet.max_row
-    for i in range(1, last_row+1):
+    for i in range(1, last_row+1): #loop through the rows
         row = []
-        for j in range(1, sheet.max_column+1):
-            cell = sheet.cell(row = i, column = j)
+        for j in range(1, sheet.max_column+1): #loop through the columns
+            cell = sheet.cell(row = i, column = j) #add the column to the rows
             row.append(cell.value)
-        out.append(row)
+        out.append(row) #add the rows to the output
 
     return out
